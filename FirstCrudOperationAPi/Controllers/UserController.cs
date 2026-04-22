45597888d6b5538c56e332b13f36d7646605c3ec
Indu Bala <indubala@gmail.com>
2026-04-22 22:01:03 +0530

    Added new changes in usercontroller

--- FirstCrudOperationAPi/Controllers/UserController.cs
+++ FirstCrudOperationAPi/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using FirstCrudOperationAPi.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace FirstCrudOperationAPi.Controllers
@@ -7,6 +8,39 @@ namespace FirstCrudOperationAPi.Controllers
     [ApiController]
     public class UserController : ControllerBase
     {
-        //this is a simple in-memory list to store users for demonstration purposes
+        private readonly ApplicationDbContext _context;
+
+        //Contructor is a special member function which is automcatically when we create an object of the class. it is used to initialize the object of the class. it has the same name as the class and it does not have a return type. it can be parameterless or it can have parameters. in this case we are using parameterized constructor to inject the dependency of the ApplicationDbContext class.
+        public UserController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        //Get : api/User/GetAll
+
+        [HttpGet("GetAll")]
+        public IActionResult GetAll()
+        {
+            var users = _context.Users.ToList();
+            return Ok(users);  //200
+        }
+        [HttpPost("Add")]
+        public IActionResult Add(User user)
+        {
+            _context.Users.Add(user);
+            _context.SaveChanges();
+            return Ok(user); //200
+            //1000
+        }
     }
 }
+
+
+// What are http methods in web development?
+//http Get
+
+
+//[httpget]-- it is used to get or retrieve data from the database .
+
+//http Post--- it is used to create or add new data to the database .
+//http Put--- it is used to update or modify existing data in the database .
+//http Delete--- it is used to delete or remove data from the database .
--- FirstCrudOperationAPi/Models/User.cs
+++ FirstCrudOperationAPi/Models/User.cs
@@ -5,6 +5,29 @@
         public int Id { get; set; }
         public string Name { get; set; }
         public string Email { get; set; }
-        public decimal Salary { get; set; }
+        //public decimal Salary { get; set; }
+
+
+
+        //public int Productname { get; set; }
     }
 }
+
+
+//Solid Principles in C#:
+
+// solid principles are a set of design principles that help developers create maintainable,
+// flexible, and scalable software. so we easily extend our code , easily maintain our code and easily test our code. these principles are widely used in object-oriented programming and are considered best practices for software development.
+//
+// The five SOLID principles are:
+
+// 1. Single Responsibility Principle (SRP):
+// A class should have only one reason to change, meaning it should have only one responsibility or job.
+
+
+// 2. Open/Closed Principle (OCP):
+// Liskov Substitution Principle (LSP):
+
+// interface Segregation Principle (ISP):
+
+// Dependency Inversion Principle (DIP):
